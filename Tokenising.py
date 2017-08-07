9ec6f7feb37b707fc62c011341e230916180f904
Pepper-13 <pepper_1317@yahoo.com>
2017-08-07 17:10:35 +0530

    Create Tokenising.py

---
+++ Tokenising.py
@@ -0,0 +1,4 @@
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+example_test= "Hello there, how are you doing today? The weather is good and Python is awesome. There are chances of rainfall and thunderfall"
+print(sent_tokenize(example_test))
